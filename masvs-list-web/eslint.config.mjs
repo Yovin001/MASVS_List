96c54354688b1768f4fde7d25f5d16a5f1d4c9da
yovin01 <stiven-yovin@hotmail.com>
2025-01-04 14:24:17 -0500

    Fix eslint config in web

--- masvs-list-web/eslint.config.mjs
+++ masvs-list-web/eslint.config.mjs
@@ -2,10 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginAstro from "eslint-plugin-astro";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,astro}"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginAstro.configs.recommended
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,astro}"],
+  languageOptions: {
+    sourceType: "commonjs",
+    globals: globals.browser,
+  },
+  extends: [
+    "eslint:recommended",
+    pluginJs.configs.recommended,
+    pluginAstro.configs.recommended,
+  ],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+  },
+};
