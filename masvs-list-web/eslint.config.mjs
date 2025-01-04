3d00c154a07d24653a7cee80bfd7a70997a53111
yovin01 <stiven-yovin@hotmail.com>
2025-01-04 13:48:19 -0500

    Fix eslint config files for web

--- masvs-list-web/eslint.config.mjs
+++ masvs-list-web/eslint.config.mjs
@@ -1,10 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ['*.astro']},
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
